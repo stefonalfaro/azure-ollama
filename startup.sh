41e5e8f9c59ac7ca4abd299120f3f47041015e27
Stefon Alfaro <stefonalfaro@gmail.com>
2023-12-03 18:27:43 -0600

    Startup to run in background with logging

--- startup.sh
+++ startup.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
-# Startup script for Azure Container Instance
 
-# Delay execution
-sleep 120
+# Log start of the script
+echo "Startup script initiated."
 
-# Run ollama command in the background
-/usr/bin/ollama run mistral
+# Running the command block in a subshell in the background
+(
+    # Log pre-delay
+    echo "Sleeping for 120 seconds before running ollama command..."
 
-# Any other startup commands can be added here
+    # Delay execution
+    sleep 120
+
+    # Log pre-execution
+    echo "Starting to run 'ollama run mistral'..."
+
+    # Run ollama command
+    /usr/bin/ollama run mistral
+
+    # Log post-execution
+    echo "'ollama run mistral' command executed."
+
+    # You can include additional commands here if needed
+) &
+
+# Log that the subshell command is running in the background
+echo "Ollama command running in the background."
+
+# Any other initialization commands can go here
+
+# Log script completion
+echo "Startup script completed."
+
+# Exit the script
+exit 0
